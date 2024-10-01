cf00ffe7c73ef1e8773468f40650da45d9610be0
Sári Bence <sari.bence.2023f@ms.gdszeged.hu>
2024-10-01 08:38:19 +0200

    elso

---
+++ 06math.py
@@ -0,0 +1,37 @@
+import math
+
+
+# print("kör terulet es kerulet")
+
+# r = int(input("addj meg egy szamot 1-10 kozott"))
+# # b = int(input(" addj meg egy szamot 1-10 kozott"))
+# # a = int(input(" addj meg egy szamot 1-10 kozott"))
+
+# kör_ker = 2*3.14
+# kör_ker1 = 2*r*(22/7)
+# kor_ker2 = 2*r*math.pi
+# print(kör_ker)
+# print(kör_ker1)
+# print(kor_ker2)
+
+# kör_ter = r*r*math.pi
+# kör_ter1 = math.pow(r,r) * math.pi
+# print(kör_ter)
+# print(kör_ter1)
+
+# print("haromszog")
+# a = int(input("addj meg egy szamot 1-10 kozott"))
+# b = int(input("addj meg egy szamot 1-10 kozott"))
+# c = int(input("addj meg egy szamot 1-10 kozott"))
+# k= a+b+c
+# print(f' A haromszog kerulete {a} es {b} es {c} {k}')
+# print("a haromszog terulete")
+# s = k/2
+# t = math.sqrt(s*(s-a)*(s-b)*(s-c))
+# print(t)
+
+print("testtomeg")
+print("testmagassag")
+testt = int(input(testtomeg))
+testm = int(input(tastmagassag))
+t1= (t/2)*2
